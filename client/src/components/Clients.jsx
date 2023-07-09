c56fba244734dc81085fb395f2b41bdc72aea4ad
aman <amanagarwalx123@gmail.com>
2023-07-09 19:46:26 +0530

    client added

---
+++ client/src/components/Clients.jsx
@@ -0,0 +1,29 @@
+import Spinner from './Spinner';
+import {useQuery} from '@apollo/client';
+import ClientRow from './ClientRow';
+import { GET_CLIENTS } from '../queries/clientQueries';
+
+export const Clients = () => {
+   const { loading,error,data } = useQuery(GET_CLIENTS);
+   if(loading) return <Spinner />
+   if(error) return <p>Something went wrong</p>;
+  return (
+    <>
+    {!loading && !error && (
+        <table className='table table-hover mt-3'>
+            <thead>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th></th>
+            </thead>
+            <tbody>
+                {data.clients.map((client)=>(
+                        <ClientRow key={client.id} client={client} />
+                ))}
+            </tbody>
+        </table>
+    )}
+    </>
+  )
+}
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import { Clients } from "../components/Clients";
+import { AddClientModal } from "../components/AddClientModal";
+import { Projects } from "../components/Projects";
+export const Home = () => {
+  return (
+    <>
+    <div className="d-flex gap-3 mb-4">
+      <AddClientModal />
+      {/* <AddProjectModal /> */}
+    </div>
+      <Projects />
+      <hr />
+      <Clients />
+    </>
+  )
+}
